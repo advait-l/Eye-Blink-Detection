2051757c4d08e83949dee960bd9c78417b23bd72
Nithin Raj <nithin.raj@iiitb.org>
2020-05-11 08:41:28 +0530

    Modified contents of fcm test

--- source/fcm_test.py
+++ source/fcm_test.py
@@ -6,7 +6,7 @@ push_service = FCMNotification(api_key="AAAAeVSHIvE:APA91bFp0ac-A95fnu1cjzZL5AFa
 # Your api-key can be gotten from:  https://console.firebase.google.com/project/<project-name>/settings/cloudmessaging
 
 registration_id = "d6j_2XWkSN-R2HI4uNRh2X:APA91bF1K1hbj0z074KzDwIJbwEXOxqu5OHfoEL2ULb9vBKWPTMLCMgvgfzy0-vm5gaVBH8tfR0_gFMFuSFzYFF2Pq-3RyDwiKlT9-apr35O3vyc7cE4z_xDGfxGwSrxRD1eWoWlLTmq"
-message_title = "Uber update"
-message_body = "Hi john, your customized news for today is ready"
+message_title = "Eye Blink Sequence Detected"
+message_body = "1001"
 result = push_service.notify_single_device(registration_id=registration_id, message_title=message_title, message_body=message_body)
 print(result)
